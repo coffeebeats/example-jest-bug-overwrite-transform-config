03ff7bfcd78c2c440b15ee0dffa22910a70e937c
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-01-13 17:41:31 -0800

    improve docs; change default config in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,28 @@
 // Instructions: Uncomment one of these at a time to observe the behavior.
 
 /*
+ * RESULT: PASS for both 'a.json' and 'b.json'
+ *
  * This *passes* as expected!
  *
- * NOTE: You can comment out one of the entries to validate that it works as
- * expected.
+ * NOTE: You can comment out one of the entries to validate that
+ * the test works as expected.
  */
-module.exports = {
-  transform: {
-    "data/a\\.json": ["./preprocessor.js", { fail: false }],
-    "data/b\\.json": ["./preprocessor.js", { fail: false }],
-  },
-};
+// module.exports = {
+//   transform: {
+//     "data/a\\.json": ["./preprocessor.js", { fail: false }],
+//     "data/b\\.json": ["./preprocessor.js", { fail: false }],
+//   },
+// };
 
 /*
+ * RESULT: FAIL for both 'a.json' and 'b.json'
+ *
  * This *throws* as expected!
  *
- * NOTE: You can comment out one of the entries to validate that it works as
- * expected.
+ * NOTE: You can comment out one of the entries to validate that
+ * the test works as expected.
  */
-
 // module.exports = {
 //   transform: {
 //     "data/a\\.json": ["./preprocessor.js", { fail: true }],
@@ -28,11 +31,14 @@ module.exports = {
 // };
 
 /*
- * This throws for 'data/a.json' when it shouldn't!
+ * (CURRENT) RESULT: FAIL for both 'a.json' and 'b.json'
+ * (EXPECTED) RESULT: FAIL for 'b.json' only
+ *
+ * This will cause the test to throw when parsing 'a.json'. It
+ * should actually succeed on 'a.json' and throw on 'b.json'.
  *
  * NOTE: Both entries must be present for the bug to manifest.
  */
-
 module.exports = {
   transform: {
     "data/a\\.json": ["./preprocessor.js", { fail: false }],
@@ -41,11 +47,15 @@ module.exports = {
 };
 
 /*
- * This should throw for 'a.json', but it doesn't!
+ * (CURRENT) RESULT: PASS for both 'a.json' and 'b.json'
+ * (EXPECTED) RESULT: FAIL for 'a.json' only
+ *
+ * This will cause the test to successfully parse both 'a.json'
+ * and 'b.json'. It actually should throw on 'a.json' and
+ * succeed on 'b.json'.
  *
  * NOTE: Both entries must be present for the bug to manifest.
  */
-
 // module.exports = {
 //   transform: {
 //     "data/a\\.json": ["./preprocessor.js", { fail: true }],
